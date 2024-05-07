b806eefc68461245f1266acbf06506f59f81bf8e
Sabari-1234 <sabarinathan5802@gmail.com>
2024-05-07 13:20:01 +0530

    cookies

--- app/profile/route.tsx
+++ app/profile/route.tsx
@@ -1,15 +1,21 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const headers1 = new Headers(req.headers);
   const headerList = headers();
+  const theme = req.cookies.get("theme");
+  cookies().set("total", "20");
+  console.log(theme);
+  console.log(cookies().get("total"));
+  undefined;
   console.log(headers1.get("authentication"));
   console.log(headerList.get("authentication"));
   console.log(headerList.get("user-agent"));
   return new Response("api sent", {
     headers: {
       "content-type": "text/plain",
+      "set-cookie": "theme=dark",
     },
   });
 };
